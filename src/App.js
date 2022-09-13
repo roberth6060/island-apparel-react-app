cd7d061438655a97ccb29bc4c3608a35542659b2
Robert Haye <roberthaye@Roberts-MBP.fritz.box>
2022-09-13 13:31:13 +0200

    homepage route added to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import Home from "./components/routes/home/home.component.jsx";
+import Home from "./components/routes/homepage/home.component";
 
 import "./App.scss";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   //Homepage Layout:
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
