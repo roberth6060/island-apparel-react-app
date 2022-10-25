15255b23347740386e17b677e43c61e99a2fb2dd
Robert Haye <roberthaye@Roberts-MBP.fritz.box>
2022-10-25 17:36:15 +0200

    updated navbar style component

--- src/routes/Navbar.jsx
+++ src/routes/Navbar.jsx
@@ -1,12 +1,17 @@
 //Fragment: A component that gets rendered to nothing when it gets mounted on the DOM:
 import { Fragment, useContext } from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 import { CartContext } from "../context/CartContext";
 import { signOutUser } from "../utils/firebase";
 import CartIcon from "../components/Cart/CartIcon";
 import CartDropDown from "../components/Cart/DropDown";
-import { NavbarContainer } from "./styles/Navbar";
+import {
+  NavbarContainer,
+  LogoContainer,
+  NavLinks,
+  NavLink,
+} from "./styles/Navbar";
 import Logo from "../assets/logo.png";
 
 const Navbar = () => {
@@ -18,65 +23,28 @@ const Navbar = () => {
   return (
     <Fragment>
       <NavbarContainer>
-        <Link className="logo-container" to="/">
-          <img src={Logo} alt="logo" className="logo" width="280" />
-        </Link>
-
-        <ul className="nav-links">
+        <LogoContainer to="/">
+          <img src={Logo} alt="logo" />
+        </LogoContainer>
+        <NavLinks>
           <input type="checkbox" id="checkbox_toggle" />
           <label htmlFor="checkbox_toggle" className="hamburger">
             &#9776;
           </label>
-          <div className="menu">
-            <li>
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li className="shop-menu">
-              <Link className="nav-link" to="/shop">
-                Shop
-              </Link>
-              <ul className="dropdown">
-                <li>
-                  <Link className="nav-link" to="/shop">
-                    All
-                  </Link>
-                </li>
-                <li>
-                  <Link className="nav-link" to="/shop">
-                    Women
-                  </Link>
-                </li>
-                <li>
-                  <Link className="nav-link" to="/shop">
-                    Men
-                  </Link>
-                </li>
-              </ul>
-            </li>
-
-            <li>
-              <Link className="nav-link" to="/contact">
-                Contact
-              </Link>
-            </li>
-            <li>
-              <Link className="nav-link" to="/auth">
-                {currentUser ? (
-                  <span className="nav-link" onClick={signOutUser}>
-                    Sign Out
-                  </span>
-                ) : (
-                  <span className="nav-link"> Sign In</span>
-                )}
-              </Link>
-            </li>
-            <li>
-              <CartIcon />
-            </li>
-          </div>
-        </ul>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/shop">Shop</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+          <NavLink to="/auth">
+            {currentUser ? (
+              <NavLink as="span" onClick={signOutUser}>
+                Sign Out
+              </NavLink>
+            ) : (
+              <NavLink as="span"> Sign In</NavLink>
+            )}
+          </NavLink>
+          <CartIcon />
+        </NavLinks>
         {
           //Short-circuit operator (components are truthy values bs it is a function)
           isCartOpen && <CartDropDown />
--- src/routes/styles/Navbar.jsx
+++ src/routes/styles/Navbar.jsx
@@ -9,144 +9,91 @@ export const NavbarContainer = styled.div`
   background-color: #fff6bf;
   height: 5vh;
   margin: 20px 0;
+
+  a {
+    text-decoration: none;
+    color: black;
+  }
+
+  input[type="checkbox"] {
+    display: none;
+  }
+  /* ====== HAMBURGER MENU ====== */
+  .hamburger {
+    display: none;
+    font-size: 24px;
+    user-select: none;
+    cursor: pointer;
+  }
+  @media (max-width: 804px) {
+    .hamburger {
+      display: block;
+    }
+  }
+`;
+
+export const NavLink = styled(Link)`
+  list-style: none;
+  display: flex;
+  align-items: center;
 `;
 
 export const LogoContainer = styled(Link)`
-  font-family: "Qwitcher Grypen", cursive;
-  font-size: 28px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  img {
+    width: 250px;
+  }
 `;
 
-// .navbar {
+export const NavLinks = styled.div`
+  display: flex;
+  font-size: 18px;
+  gap: 1em;
+  z-index: 1;
+  align-items: center;
 
-//   a {
-//     text-decoration: none;
-//     color: black;
-//   }
-//   li {
-//     list-style: none;
-//     display: flex;
-//     align-items: center;
-//   }
-//   /* ====== LOGO ====== */
-//   #logo {
-//
-//   }
+  a {
+    padding: 5px;
+    min-width: 3.5em;
+    height: 35px;
+    font-size: 1em;
+    text-align: center;
+    justify-content: center;
 
-//   /* ====== NAVBAR MENU ====== */
-//   .menu {
-//     display: flex;
-//     font-size: 18px;
-//     gap: 1em;
-//     z-index: 1;
+    &:hover {
+      background-color: rgba(255, 0, 0, 0.8);
 
-//     a {
-//       padding: 5px;
-//       min-width: 3.5em;
-//       font-size: 1.2em;
-//       text-align: center;
+      transition: 0.5s ease;
+    }
+  }
 
-//       &:hover {
-//         background-color: rgba(255, 0, 0, 0.8);
+  @media (max-width: 804px) {
+    top: 60px;
+    display: none;
+    position: absolute;
+    right: 0;
+    left: 0;
+    text-align: center;
+    padding: 16px 0;
+    margin: 29px;
 
-//         transition: 0.5s ease;
-//       }
-//     }
-//     /* ====== DROPDOWN MENU ====== */
-//     .shop-menu {
-//       position: relative;
+    a {
+      text-align: left;
+    }
 
-//       &:hover {
-//         .dropdown {
-//           //Will impliment feature later
-//           display: none;
-//           z-index: 1;
-//         }
-//       }
+    li {
+      a:hover {
+        display: inline-block;
+        transition: 0.3s ease;
+      }
+    }
 
-//       .dropdown {
-//         background-color: #fff6bf;
+    li + li {
+      margin-top: 12px;
+      width: auto;
+    }
 
-//         display: none;
-//         padding: 1em 1em;
-//         position: absolute;
-//         top: 40px;
-
-//         li + li {
-//           margin-top: 10px;
-//         }
-//         li {
-//           padding: 0.5em 1em;
-//           text-align: center;
-//           width: 6em;
-//         }
-//       }
-//     }
-//   }
-//   /* ====== CHECKBOX DISPLAY NONE ====== */
-//   input[type="checkbox"] {
-//     display: none;
-//   }
-//   /* ====== HAMBURGER MENU ====== */
-//   .hamburger {
-//     display: none;
-//     font-size: 24px;
-//     user-select: none;
-//     cursor: pointer;
-//   }
-// }
-
-// /* ================== MOBILE VIEW MEDIA QUERIES ================== */
-// @media (max-width: 804px) {
-//   /* ====== NAVBAR MENU ====== */
-//   body:hover {
-//     .menu {
-//       display: none;
-//     }
-//   }
-//   .navbar {
-//     .menu {
-//       background-color: #fff6bf;
-//       top: 60px;
-//       display: none;
-//       position: absolute;
-//       right: 0;
-//       left: 0;
-//       text-align: center;
-//       padding: 16px 0;
-//       margin: 29px;
-
-//       a {
-//         text-align: left;
-//       }
-
-//       li {
-//         a:hover {
-//           display: inline-block;
-//           transition: 0.3s ease;
-//         }
-//       }
-
-//       li + li {
-//         margin-top: 12px;
-//         width: auto;
-//       }
-//     }
-
-//     .hamburger {
-//       display: block;
-//     }
-
-//     .dropdown {
-//       left: 36px !important;
-//       top: 5px !important;
-//       transform: translate(35%);
-//     }
-
-//     input[type="checkbox"]:checked ~ .menu {
-//       display: block;
-//     }
-//   }
-// }
+    .hamburger {
+      display: block;
+    }
+  }
+`;
