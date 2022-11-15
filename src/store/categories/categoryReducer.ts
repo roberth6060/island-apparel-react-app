83323b9e60355f36f1fb2ce92bf9171664399937
Robert C. Haye <roberthaye1994@gmail.com>
2022-11-15 10:45:39 +0100

    converted categories reducer to typescript

--- src/store/categories/categoryReducer.ts
+++ src/store/categories/categoryReducer.ts
@@ -1,8 +1,13 @@
-import { CATEGORY_ACTION_TYPES } from "./categoryTypes";
+import { Category, CATEGORY_ACTION_TYPES } from "./categoryTypes";
 import { CategoryAction } from "./categoryAction";
 /**NOTE - CATEGORY REDUCER THAT TRACKS USER CURRENT STATE  */
 
-const CATEGORIES_INITIAL_STATE = {
+export type CategoriesState = {
+  readonly categoriesArray: Category[];
+  readonly isLoading: boolean;
+  readonly error: Error | null;
+};
+const CATEGORIES_INITIAL_STATE: CategoriesState = {
   categoriesArray: [],
   isLoading: false,
   error: null,
@@ -12,14 +17,13 @@ export const categoryReducer = (
   state = CATEGORIES_INITIAL_STATE,
   action = {} as CategoryAction // discriminated unions (3 types)
 ) => {
-  const { type, payload } = action;
-  switch (type) {
+  switch (action.type) {
     case CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_START:
       return { ...state, isLoading: true };
     case CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return { ...state, categoriesArray: payload, isLoading: false };
+      return { ...state, categoriesArray: action.payload, isLoading: false };
     case CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return { ...state, error: payload, isLoading: false };
+      return { ...state, error: action.payload, isLoading: false };
     default:
       return state;
   }
