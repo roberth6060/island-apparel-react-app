4326d0e491b1799499c08ce216479bd921dbd358
Robert C. Haye <roberthaye1994@gmail.com>
2023-01-01 15:08:04 +0100

    finished navbar desktop view style component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,3 @@
-//Fragment: A component that gets rendered to nothing when it gets mounted on the DOM:
 import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -6,34 +5,32 @@ import CartIcon from "../common/Cart/CartIcon";
 import CartDropDown from "../common/Cart/DropDown";
 import {
   NavbarContainer,
-  LogoContainer,
+  Logo,
   NavLink,
   Menu,
+  MenuToggle,
+  MenuButtonContainer,
+  MenuButton,
 } from "./style/Navbar";
-import Logo from "../../assets/logo.png";
 import { selectCurrentUser } from "../../store/user/userSelector";
 import { selectIsCartOpen } from "../../store/cart/cartSelector";
 import { signOutStart } from "../../store/user/userAction";
 
 const Navbar = () => {
   const dispatch = useDispatch();
-
   const currentUser = useSelector(selectCurrentUser);
   //Conditionally render cart drop based on  isCartOpen
   const isCartOpen = useSelector(selectIsCartOpen);
-
   const signOutUser = () => dispatch(signOutStart());
 
   return (
     <Fragment>
       <NavbarContainer>
-        <LogoContainer to="/">
-          <img src={Logo} alt="logo" />
-        </LogoContainer>
-        <input id="menu-toggle" type="checkbox" />
-        <label className="menu-button-container" htmlFor="menu-toggle">
-          <span className="menu-button"></span>
-        </label>
+        <Logo/>
+        <MenuToggle/>
+        <MenuButtonContainer>
+          <MenuButton/>
+        </MenuButtonContainer>
         <Menu>
           <NavLink to="/">Home</NavLink>
           <NavLink to="/shop">Shop</NavLink>
--- src/components/Navbar/style/Navbar.tsx
+++ src/components/Navbar/style/Navbar.tsx
@@ -1,7 +1,76 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import MyLogo  from "../../../assets/logo.png"
 
+export const Logo = styled.img`
+  width: 230px;
+`
+Logo.defaultProps = {
+  src:MyLogo,
+}
+export const MenuButton = styled.span`
+&, 
+&::before,
+&::after {
+  display: block;
+  background-color: #4e6c50;
+  position: absolute;
+  height: 4px;
+  width: 30px;
+  transition: transform 400ms cubic-bezier(0.23, 1, 0.32, 1);
+  border-radius: 2px;
+}
+&::before {
+  content: "";
+  margin-top: -8px;
+}
+&::after {
+  content: "";
+  margin-top: 8px;
+}
+`
+export const MenuButtonContainer = styled.label.attrs({for: 'menu-toggle'})`
+ display: none;
+  height: 100%;
+  width: 30px;
+  cursor: pointer;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
+@media screen and (max-width:800px) {
+  display: flex;
+}
+`
+
+export const MenuToggle = styled.input.attrs({ id: 'menu-toggle',type: 'checkbox' })`
+  &:checked + MenuButtonContainer MenuButton::before {
+    margin-top: 0px;
+    transform: rotate(405deg);
+  }
+  &:checked + MenuButtonContainer MenuButton::after {
+  margin-top: 0px;
+  transform: rotate(-405deg);
+}
+
+  @media screen and (max-width:800px) {
+   
+    & ~ a {
+    height: 0;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+  }
+  &:checked ~ a {
+    border: 1px solid #333;
+    height: 2.5em;
+    padding: 0.5em;
+    transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+  }
+  }
+
+`
 export const NavbarContainer = styled.div`
   align-items: center;
   display: flex;
@@ -11,65 +80,20 @@ export const NavbarContainer = styled.div`
   height: 5vh;
   margin: 20px 0;
 
-  .menu-button-container {
-  display: none;
-  height: 100%;
-  width: 30px;
-  cursor: pointer;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
 #menu-toggle {
   display: none;
 }
 
-.menu-button,
-.menu-button::before,
-.menu-button::after {
-  display: block;
-  background-color: #4e6c50;
-  position: absolute;
-  height: 4px;
-  width: 30px;
-  transition: transform 400ms cubic-bezier(0.23, 1, 0.32, 1);
-  border-radius: 2px;
-}
 
-.menu-button::before {
-  content: "";
-  margin-top: -8px;
-}
-
-.menu-button::after {
-  content: "";
-  margin-top: 8px;
-}
-
-#menu-toggle:checked + .menu-button-container .menu-button::before {
-  margin-top: 0px;
-  transform: rotate(405deg);
-}
 
 #menu-toggle:checked + .menu-button-container .menu-button {
   background: rgba(255, 255, 255, 0);
 }
 
-#menu-toggle:checked + .menu-button-container .menu-button::after {
-  margin-top: 0px;
-  transform: rotate(-405deg);
-}
 
-  @media screen and (max-width:800px) {
-      .menu-button-container {
-      display: flex;
-    }
-  }
 
   
 `;
-
 
 export const NavLink = styled(Link)`
   list-style: none;
@@ -77,20 +101,7 @@ export const NavLink = styled(Link)`
   align-items: center;
 `;
 
-export const LogoContainer = styled(Link)`
-    display: flex;
-    gap: 5px;
-    align-items: center;
 
-    img {
-      width: 50px;
-    }
-    span {
-      font-size: 17px;
-      font-family: "Raleway";
-      color: #0a2c46;
-    }
-`;
 
 export const Menu = styled.div`
   display: flex;
@@ -131,42 +142,6 @@ a {
     flex-direction: column;
     width: 100%;
     justify-content: center;
-
-    #menu-toggle ~ li {
-    height: 0;
-    margin: 0;
-    padding: 0;
-    border: 0;
-    transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
-  }
-  #menu-toggle:checked ~ li {
-    border: 1px solid #333;
-    height: 2.5em;
-    padding: 0.5em;
-    transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
-  }
-
-  .menu > li {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0;
-    padding: 0.5em 0;
-    width: 100%;
-    color: red;
-    background-color: $bg;
-  }
-  .menu > li:not(:last-child) {
-    border-bottom: 1px solid #444;
-  }
-
-  .menu {
-    li {
-      &:hover:not(.active) {
-        transform: skew(0deg);
-      }
-    }
-  }
 }
 
 `;
